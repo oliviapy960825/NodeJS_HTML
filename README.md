298888f80422d90b30447ab5eb31c7fc0dc2ec4e
Wang Peiyu <oliviapy@udel.edu>
2020-10-13 22:49:22 -0400

    added first NodeJS Server to host html pages

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # NodeJS_HTML
+
+### 10/13/2020 updates
+#### First HTML page with NodeJS Server
+contents:
+2 html pages, one nodejs file for localhost Server
---
+++ ServerExample.js
@@ -0,0 +1,56 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+
+
+var server = http.createServer(function(request, response){
+  var path = url.parse(request.url).pathname;
+  switch(path){
+    case '/':
+      response.writeHead(200, {
+        'Content-Type' : 'text/plain'
+      });
+      response.write("This is the message");
+      response.end();
+      break;
+    case '/Html1.html':
+      fs.readFile(__dirname + path, function(error, data){
+        if (error){
+          response.writeHead(404);
+          response.write(error);
+          response.end();
+        }
+        else{
+          response.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+          response.write(data);
+          response.end();
+        }
+      });
+      break;
+    case '/Html2.html':
+      fs.readFile(__dirname + path, function(error, data){
+        if(error){
+          response.writeHead(404);
+          response.write(error);
+          response.end();
+        }
+        else{
+          response.writeHead(200,{
+            'Content-Type': 'text/html'
+          });
+          response.write(data);
+          response.end();
+        }
+      });
+      break;
+    default:
+      response.writeHead(404);
+      response.write("This page does not exist");
+      response.end();
+      break;
+  }
+});
+
+server.listen(8082);
